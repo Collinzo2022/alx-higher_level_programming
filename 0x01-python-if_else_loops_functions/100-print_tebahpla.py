47b2a741c46d6e0efab457deeaa86a2a7e8afa33
Collinzo2022 <aefuetanu@gmail.com>
2024-04-09 06:59:55 -0700

    love

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
+for i in range(26):
     if i % 2 == 0:
-        print(chr(i), end="")
+        print('{:c}'.format(122 - i), end='')
     else:
-        print(chr(i - 32), end="")
+        print('{:c}'.format(90 - i), end='')
