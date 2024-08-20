89ece21b87dd0a7070a483917c20b2905ac9cbe2
Collinzo2022 <aefueatnu@gmail.com>
2024-08-20 07:02:38 -0700

    add

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,8 @@
 
 // Create an aray with the three strings
 const messages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-// Use a for loop to iterate through the array and print each message
-for (let i = 0; i < messages.length; i++) {
-  console.log(messages[i]);
+
+// loop through the array
+for (const message of messages) {
+  console.log(message);
 }
