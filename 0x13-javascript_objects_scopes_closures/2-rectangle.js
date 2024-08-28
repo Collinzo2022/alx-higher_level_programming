6bb949dfc3de4a8108ca54c1478b752c6924a141
Collinzo2022 <aefueatnu@gmail.com>
2024-08-28 07:29:43 -0700

    love

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    /* nitialize the instance attributes width and height */
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.export = Rectangle;
