9e54a523dbb6547efa90426a3baffbfff0f4ba95
Collinzo2022 <aefueatnu@gmail.com>
2024-08-30 04:42:50 -0700

    add

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,12 @@
 
 class Rectangle {
   constructor (w, h) {
-    /* nitialize the instance attributes width and height */
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {};
+    }
   }
 }
-
 module.export = Rectangle;
